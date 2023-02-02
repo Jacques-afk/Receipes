cfc07aa9747ccb5ce42ec5b89415f2997f8139af
jacques <skydive651@gmail.com>
2023-02-02 15:55:27 +0800

    post login details to restdb server

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 
 $(document).ready(function (){
-    const APIKEY = "";              
-    getContacts();
-    $("#update-contact-container").hide();
-    $("#add-update-msg").hide();
+    const APIKEY = "63db64973bc6b255ed0c456e";              
 
     $('#login_Btn').click(function(e){
         e.preventDefault();
         //console.log("hello")
         let login_Username = $("#login_Username").val();
-        let login_Password = $('login_Password').val();
-
-        // uses this to create key value send the storage
+        let login_Password = $('#login_Password').val();
 
         let particulars = {
-            "name": login_Username,
-            "password":login_Password,       
-        }
+            "Username": login_Username,
+            "Password":login_Password,       
+        };
 
         let settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "",
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles",
+        "method": "POST",
+        "headers": {
+          "content-type": "application/json",
+          "x-apikey": APIKEY,
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(particulars)
+      }
 
-         
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
 
-        }
-
-
-    })
-}
+      })
+  })
+        
