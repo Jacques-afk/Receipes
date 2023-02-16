9d031c07dd9b4ac927ce1b5f4c9c448ff5d07d0b
jacques <skydive651@gmail.com>
2023-02-17 03:07:31 +0800

    connecting these 4 tgt to show results

--- postresults.js
+++ postresults.js
@@ -3,8 +3,10 @@
 
 const APIKEY = "63db64973bc6b255ed0c456e";                      
 let counter = 0;
-let catergory_Search = localStorage.setItem("Category", "quick")
-let category_Search = localStorage.getItem("Category")
+let category_Search = localStorage.getItem("Category");
+let manual_Search = localStorage.getItem("Dishs");
+console.log(manual_Search);
+
 
 function getquickrecipes(callback){
     console.log('create')
@@ -22,6 +24,7 @@ function getquickrecipes(callback){
     
     $.ajax(settings).done(function (response) {
         for (let i = 0; i < response.length; i++){
+
             let time = response[i].Time;
             if (time == 5){
                 console.log('found a post captain');
@@ -41,7 +44,286 @@ function quickrecipes(){
         let photoSRC = post.Photo[0]
         console.log(photoSRC)
     
-        $("<div class=Dish><h2 class=DishName>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1> <img src=https://tutorial-9477.restdb.io/media/" + photoSRC + "><button class=Go" + counter + ">button</button></div>").insertAfter("br.start");
+        $("<div class=Dish><h2 class=DishName>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1> <img class=JAJABINKS src=https://tutorial-9477.restdb.io/media/" + photoSRC + "><button class=Go" + counter + ">button</button></div>").insertAfter(".gobacklink");
+        console.log("One dish done");
+        counter += 1;
+        console.log(counter);
+    
+        $('button').click(function(o){
+            let classy = $(this)[0].className;
+    
+            let targetted_div = ($("." + classy).parent()[0]);   
+            let targetted_dishname = targetted_div.children[0].innerText;
+            let targetted_authorname = targetted_div.children[1].innerText;
+            localStorage.setItem("Dishname", targetted_dishname);
+            window.location.href = "recipedetails.html";              
+        })  
+    })
+} 
+
+function getfavouriterecipes(callback){
+    console.log('create')
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tutorial-9477.restdb.io/rest/recipesposts",
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        for (let i = 0; i < response.length; i++){
+            
+            let foodcategory = response[i].Category;
+
+
+            if (foodcategory == "Vietnamese"){
+                console.log('found a post captain');
+                let post = response[i];
+                callback(post)
+            }
+        }
+    });
+}
+
+function favouriterecipes(){
+    getfavouriterecipes(function(post){
+        console.log(post);
+    
+        let NameOfDish = post.NameDish;
+        let Author = post.Author;
+        let photoSRC = post.Photo[0]
+        console.log(photoSRC)
+    
+        $("<div class=Dish><h2 class=DishName>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1> <img class=JAJABINKS src=https://tutorial-9477.restdb.io/media/" + photoSRC + "><button class=Go" + counter + ">button</button></div>").insertAfter(".gobacklink");
+        console.log("One dish done");
+        counter += 1;
+        console.log(counter);
+    
+        $('button').click(function(o){
+            let classy = $(this)[0].className;
+    
+            let targetted_div = ($("." + classy).parent()[0]);   
+            let targetted_dishname = targetted_div.children[0].innerText;
+            let targetted_authorname = targetted_div.children[1].innerText;
+            localStorage.setItem("Dishname", targetted_dishname);
+            window.location.href = "recipedetails.html";              
+        })  
+    })
+}
+
+///////////////////////////////////////////////////////////
+
+
+function getchineserecipes(callback){
+    console.log('create')
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tutorial-9477.restdb.io/rest/recipesposts",
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        for (let i = 0; i < response.length; i++){
+            
+            let foodcategory = response[i].Category;
+            if (foodcategory == "Chinese"){
+                console.log('found a post captain');
+                let post = response[i];
+                callback(post)
+            }
+        }
+    });
+}
+
+function chineserecipes(){
+    getchineserecipes(function(post){
+        console.log(post);
+    
+        let NameOfDish = post.NameDish;
+        let Author = post.Author;
+        let photoSRC = post.Photo[0]
+        console.log(photoSRC)
+    
+        $("<div class=Dish><h2 class=DishName>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1> <img class=JAJABINKS src=https://tutorial-9477.restdb.io/media/" + photoSRC + "><button class=Go" + counter + ">button</button></div>").insertAfter(".gobacklink");
+        console.log("One dish done");
+        counter += 1;
+        console.log(counter);
+    
+        $('button').click(function(o){
+            let classy = $(this)[0].className;
+    
+            let targetted_div = ($("." + classy).parent()[0]);   
+            let targetted_dishname = targetted_div.children[0].innerText;
+            let targetted_authorname = targetted_div.children[1].innerText;
+            localStorage.setItem("Dishname", targetted_dishname);
+            window.location.href = "recipedetails.html";              
+        })  
+    })
+} 
+
+///////////////////////////////////////////////////////////
+
+
+function getwesternrecipes(callback){
+    console.log('create')
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tutorial-9477.restdb.io/rest/recipesposts",
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        for (let i = 0; i < response.length; i++){
+            
+            let foodcategory = response[i].Category;
+            if (foodcategory == "Western"){
+                console.log('found a post captain');
+                let post = response[i];
+                callback(post)
+            }
+        }
+    });
+}
+
+function westernrecipes(){
+    getwesternrecipes(function(post){
+        console.log(post);
+    
+        let NameOfDish = post.NameDish;
+        let Author = post.Author;
+        let photoSRC = post.Photo[0]
+        console.log(photoSRC)
+    
+        $("<div class=Dish><h2 class=DishName>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1> <img class=JAJABINKS src=https://tutorial-9477.restdb.io/media/" + photoSRC + "><button class=Go" + counter + ">button</button></div>").insertAfter(".gobacklink");
+        console.log("One dish done");
+        counter += 1;
+        console.log(counter);
+    
+        $('button').click(function(o){
+            let classy = $(this)[0].className;
+    
+            let targetted_div = ($("." + classy).parent()[0]);   
+            let targetted_dishname = targetted_div.children[0].innerText;
+            let targetted_authorname = targetted_div.children[1].innerText;
+            localStorage.setItem("Dishname", targetted_dishname);
+            window.location.href = "recipedetails.html";              
+        })  
+    })
+} 
+
+///////////////////////////////////////////////////////////
+
+
+function getitalianrecipes(callback){
+    console.log('create')
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tutorial-9477.restdb.io/rest/recipesposts",
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        for (let i = 0; i < response.length; i++){
+            
+            let foodcategory = response[i].Category;
+            if (foodcategory == "Italian"){
+                console.log('found a post captain');
+                let post = response[i];
+                callback(post)
+            }
+        }
+    });
+}
+
+function italianrecipes(){
+    getitalianrecipes(function(post){
+        console.log(post);
+    
+        let NameOfDish = post.NameDish;
+        let Author = post.Author;
+        let photoSRC = post.Photo[0]
+        console.log(photoSRC)
+    
+        $("<div class=Dish><h2 class=DishName>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1> <img class=JAJABINKS src=https://tutorial-9477.restdb.io/media/" + photoSRC + "><button class=Go" + counter + ">button</button></div>").insertAfter(".gobacklink");
+        console.log("One dish done");
+        counter += 1;
+        console.log(counter);
+    
+        $('button').click(function(o){
+            let classy = $(this)[0].className;
+    
+            let targetted_div = ($("." + classy).parent()[0]);   
+            let targetted_dishname = targetted_div.children[0].innerText;
+            let targetted_authorname = targetted_div.children[1].innerText;
+            localStorage.setItem("Dishname", targetted_dishname);
+            window.location.href = "recipedetails.html";              
+        })  
+    })
+} 
+
+///////////////////////////////////////////////////////////
+
+
+function getvietnameserecipes(callback){
+    console.log('create')
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tutorial-9477.restdb.io/rest/recipesposts",
+        "method": "GET",
+        "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        for (let i = 0; i < response.length; i++){
+            
+            let foodcategory = response[i].Category;
+            if (foodcategory == "Vietnamese"){
+                console.log('found a post captain');
+                let post = response[i];
+                callback(post)
+            }
+        }
+    });
+}
+
+function vietnameserecipes(){
+    getvietnameserecipes(function(post){
+        console.log(post);
+    
+        let NameOfDish = post.NameDish;
+        let Author = post.Author;
+        let photoSRC = post.Photo[0]
+        console.log(photoSRC)
+    
+        $("<div class=Dish><h2 class=DishName>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1> <img class=JAJABINKS src=https://tutorial-9477.restdb.io/media/" + photoSRC + "><button class=Go" + counter + ">button</button></div>").insertAfter(".gobacklink");
         console.log("One dish done");
         counter += 1;
         console.log(counter);
@@ -59,15 +341,107 @@ function quickrecipes(){
 } 
 
 
+
+function searchbar(){
+    console.log(manual_Search)
+
+    let listof_dishes = manual_Search.split(",")
+    
+
+    for (let i = 0; i < (listof_dishes.length); i++){
+
+        function getting (callback){
+            let NameOfDishFound = listof_dishes[i];
+
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://tutorial-9477.restdb.io/rest/recipesposts",
+                "method": "GET",
+                "headers": {
+                "content-type": "application/json",
+                "x-apikey": APIKEY,
+                "cache-control": "no-cache"
+                }
+            }
+            
+            $.ajax(settings).done(function (response) {
+                for (let i = 0; i < response.length; i++){
+                    if (response[i].NameDish == NameOfDishFound){
+                        console.log('dish found in database')
+
+                        let NameofDishT = response[i].NameDish;
+                        let AuthorT = response[i].Author; 
+                        let photoSRCT = response[i].Photo[0]
+                
+                        callback(NameofDishT, AuthorT, photoSRCT)
+                    }
+                }
+            });
+        }
+        
+        getting(function(NameDishT, AuthorT, photoSRCT){
+            $("<div class=Dish><h2 class=DishName>" + NameDishT + "</h2><h1 class=Author_Dish>" + AuthorT + "</h1> <img class=JAJABINKS src=https://tutorial-9477.restdb.io/media/" + photoSRCT + "><button class=Go" + counter + ">button</button></div>").insertAfter(".gobacklink");
+            console.log("One dish done");
+            counter += 1;
+            console.log(counter);
+        
+            // $('button').click(function(o){
+            //     let classy = $(this)[0].className;
+        
+            //     let targetted_div = ($("." + classy).parent()[0]);   
+            //     let targetted_dishname = targetted_div.children[0].innerText;
+            //     let targetted_authorname = targetted_div.children[1].innerText;
+            //     localStorage.setItem("Dishname", targetted_dishname);
+            //     window.location.href = "recipedetails.html";              
+            // })
+        })
+
+
+
+
+
+    }
+};
+
+
+
+
+
+
 if (category_Search == "quick"){
     quickrecipes();
+    localStorage.removeItem("Category")
 }
     else if (category_Search == "favourite"){
         favouriterecipes();
+        localStorage.removeItem("Category")
     }   
     else if (category_Search == "fusion"){
         fusionrecipes();
+        localStorage.removeItem("Category")
     }
     else if (category_Search == "latest"){
         latestrecipes();
+        localStorage.removeItem("Category")
+    }
+    else if (category_Search == "chinese"){
+        chineserecipes();
+        localStorage.removeItem("Category")
+    }
+    else if (category_Search == "western"){
+        westernrecipes();
+        localStorage.removeItem("Category")
+    }
+    else if (category_Search == "vietnamese"){
+        vietnameserecipes();
+        localStorage.removeItem("Category")
+    }
+    else if (category_Search == "italian"){
+        italianrecipes();
+        localStorage.removeItem("Category")
+    }
+    else if (manual_Search.length >= 1){
+        searchbar();
+        localStorage.removeItem("Dishs")
     }
--- search.js
+++ search.js
@@ -1,13 +1,28 @@
+
 const APIKEY = '63db64973bc6b255ed0c456e';
 const search_Bar = document.querySelector('.searchhh')
 
 //section for if they press the cultural food categories//
 
+$('.chineseman').click(function(e){
+  localStorage.setItem("Category", "chinese")
+  window.location.href = "searchresults.html"
+})
 
+$('.westernman').click(function(e){
+  localStorage.setItem("Category", "western")
+  window.location.href = "searchresults.html"
+})
 
+$('.vietnamman').click(function(e){
+  localStorage.setItem("Category", "vietnamese")
+  window.location.href = "searchresults.html"
+})
 
-
-
+$('.italianman').click(function(e){
+  localStorage.setItem("Category", "italian")
+  window.location.href = "searchresults.html"
+})
 
 //section for if they search via the search bar//
 
@@ -18,8 +33,8 @@ search_Bar.addEventListener("keypress", function(event) {
       var search_Response = $('.searchhh').val(); //whatever the user typed
       let final_search = search_Response.toUpperCase();
 
-      $('.seatch').click()
-      console.log('enter pressed')
+      // $('.seatch').click();
+      console.log('enter pressed');
 
       var settings = {
         "async": true,
@@ -43,21 +58,15 @@ search_Bar.addEventListener("keypress", function(event) {
             let final_found = nameofpost.toUpperCase();
 
             if (final_found == final_search){
-
+              replieddishes.push(nameofpost);
             }
-            
-            
-    
-            
-
         }
 
+        localStorage.setItem("Dishs", replieddishes);
+        window.location.href = "searchresults.html";
 
       });
-    }
-
-
-
+     }
 }); //end of search event
 
 
