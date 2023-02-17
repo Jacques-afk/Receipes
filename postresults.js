11a23b7a775efaf5969440fadf5a40a78a9d4f03
jacques <skydive651@gmail.com>
2023-02-18 02:05:56 +0800

    custom colors for post type by category

--- postresults.js
+++ postresults.js
@@ -8,8 +8,8 @@ let manual_Search = localStorage.getItem("Dishs");
 const user = localStorage.getItem("Username");
 console.log(manual_Search);
 
-
 function pointsupdater(){
+
     var total_points = 0;
   
     var settings = {
@@ -23,114 +23,106 @@ function pointsupdater(){
         "cache-control": "no-cache"
       }
     }
-
+  
     $.ajax(settings).done(function (response) {   //checking which posts are by the current logged in user
-
-      for (let i = 0; i < response.length; i++){
-        let current_User = user;
+  
+      for (let i = 0; i < response.length; i++){   
         post_author = response[i].Author;
-
-        if (current_User == post_author){
+  
+        if (user == post_author){
+          console.log("Current user in session is " + user)
           let post_likes = response[i].Likes;
-          let post_comments = response[i].Comments_No
-
+  
           let starter = 0;
           let award_5 = 0;
           let award_10 = 0;
-
+  
           if (post_likes >= 2){
             starter += 5;
           }
-
+  
           if (post_likes > 5){
             let remain1 = post_likes % 5;
             let final1 = post_likes - remain1;
             let total_5 = final1 / 5 ;
-
+  
             let result_5 = total_5 * 5;
             award_5 += result_5;
           }
-
+  
           if (post_likes > 10){
             let remain2 = post_likes % 10;
             let final2 = post_likes - remain2;
             let total_10 = final2 / 10;
-
+  
             let result_10 = total_10 * 2;
             award_10 += result_10;
           }
-
+  
           let tally = award_5 + award_10 + starter;
-
-
+  
+  
           total_points += tally;
         }
       }  //outside the for loop
-    });
-    
-    console.log(total_points)
-
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles",
-      "method": "GET",
-      "headers": {
-        "content-type": "application/json",
-        "x-apikey": APIKEY,
-        "cache-control": "no-cache"
-      }
-    }
-
-    $.ajax(settings).done(function (response) {
-      for (let i = 0; i < response.length; i++){
-
-        if (response[i].Username == user){
-          console.log('userfound');
-
-          let userid = response[i]._id;
-          let UsernameFOR = response[i].Username;
-          let PasswordFOR = response[i].Password;
-          let EmailFOR = response[i].Email;
-          
-          var jsondata = {"Username": UsernameFOR,
-                    "Password": PasswordFOR,
-                    "Email": EmailFOR,
-                    "Points": total_points,
-                    };
-
-          var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles/" + userid,
-            "method": "PUT",
-            "headers": {
-              "content-type": "application/json",
-              "x-apikey": APIKEY,
-              "cache-control": "no-cache"
-            },
-            "processData": false,
-            "data": JSON.stringify(jsondata)
-          }
-
-          $.ajax(settings).done(function (response) {
-            console.log(response);
-          });
-
-          $('.currency').text(total_points)
-
+      console.log(total_points)
+  
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles",
+        "method": "GET",
+        "headers": {
+          "content-type": "application/json",
+          "x-apikey": APIKEY,
+          "cache-control": "no-cache"
         }
       }
+    
+      $.ajax(settings).done(function (response) {
+        for (let i = 0; i < response.length; i++){
+    
+          if (response[i].Username == user){
+            console.log('userfound');
+    
+            let userid = response[i]._id;
+            let UsernameFOR = response[i].Username;
+            let PasswordFOR = response[i].Password;
+            let EmailFOR = response[i].Email;
+            
+            var jsondata = {"Username": UsernameFOR,
+                      "Password": PasswordFOR,
+                      "Email": EmailFOR,
+                      "Points": total_points,
+                      };
+    
+            var settings = {
+              "async": true,
+              "crossDomain": true,
+              "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles/" + userid,
+              "method": "PUT",
+              "headers": {
+                "content-type": "application/json",
+                "x-apikey": APIKEY,
+                "cache-control": "no-cache"
+              },
+              "processData": false,
+              "data": JSON.stringify(jsondata)
+            }
+    
+            $.ajax(settings).done(function (response) {
+              console.log(response);
+            });
+    
+            $('.currency').text(total_points)
+    
+          }
+        }
+      });
     });
   }
     
-  pointsupdater();
-
-
-
-
-
-
+pointsupdater();
 function getquickrecipes(callback){
     console.log('create')
     var settings = {
@@ -299,7 +291,7 @@ function fusionrecipes(){
 //////////////////////////////////////////////////////////////////
 
 function getlatestrecipes(callback){
-    console.log('create')
+    // console.log('create')
     var settings = {
         "async": true,
         "crossDomain": true,
@@ -316,23 +308,23 @@ function getlatestrecipes(callback){
 
     let month = today.getMonth() + 1;
     let year = today.getFullYear();
-    let date = today.getDay() + 12;
-
+    let todaydate = today.getDay() + 12;
+    console.log(todaydate)
+                                                                       //today date -7 give it a span of a week
     $.ajax(settings).done(function (response) {
-        for (let i = 0; i < response.length; i++){
+        for (let p = 0; p < response.length; p++){
 
-            let dateyposty = response[i].Date_of_Post;
-            emptyi = []
-            emptyi.push = dateyposty.split("/");
-            console.log(emptyi[0])
+            let dateyposty = response[p].Date_of_Post;
+            let date = dateyposty.split("/")[0];                        //the day date of the current day
+            console.log(date)
 
-            let re_str = date - 7;
+            let re_str = (todaydate - 7);                               // today is the 18th, so from 11-18 (range)
 
-            for (let i = re_str; i <= date; i++){
+            for (let i = re_str; i <= date; i++){                       //i is 11, as long as 10 <= 17, i go up each time
 
-                if (i == dateyposty){
+                if (i == date){                                         // i is checking thro the range going up
                     console.log('found a post captain');
-                    let post = response[i];
+                    let post = response[p];
                     callback(post)
                 }
                     else{
@@ -343,7 +335,6 @@ function getlatestrecipes(callback){
     });
 }
     
-
 function latestrecipes(){
     getlatestrecipes(function(post){
         console.log(post);
@@ -351,21 +342,46 @@ function latestrecipes(){
         let NameOfDish = post.NameDish;
         let Author = post.Author;
         let photoSRC = post.Photo[0]
-        let categoryforicon = post.Category
-        console.log(photoSRC)
+        let categoryforicon = post.Category;
+        console.log(photoSRC);
     
-        $("<div class=Dish><h2 class=DishName>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1> <img class=JAJABINKS src=https://tutorial-9477.restdb.io/media/" + photoSRC + "><button class=Go" + counter + ">button</button> <img class=iconcategory src=/" + categoryforicon + ".png> </div>").insertAfter(".gobacklink");
+        $("<div class=Dish><h2 class=DishName" + counter + ">" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1> <img class=JAJABINKS src=https://tutorial-9477.restdb.io/media/" + photoSRC + "><button class=Go" + counter + ">button</button> <img class=iconcategory src=/" + categoryforicon + ".png> </div>").insertAfter(".gobacklink");
         console.log("One dish done");
+        
+        let colorstyle = '';
+
+        if (categoryforicon == 'Chinese'){
+            colorstyle = 'red';
+        }
+            else if (categoryforicon == 'Western'){
+                colorstyle = '#2E1503';
+            }
+            else if (categoryforicon == 'Vietnamese'){
+                colorstyle = 'orange';
+            }
+            else if (categoryforicon == 'Italian'){
+                colorstyle = 'green';
+            }
+
+        $('.Dish').css({
+            'background-color': 'grey',
+        })
+        $('.DishName' + counter).css({
+            'color': colorstyle,
+        })
+        $('.Go' + counter).css({
+            'background-color': colorstyle,
+            'color': 'white',
+        })
         counter += 1;
-        console.log(counter);
-    
+
         $('button').click(function(o){
             let classy = $(this)[0].className;
     
             let targetted_div = ($("." + classy).parent()[0]);   
             let targetted_dishname = targetted_div.children[0].innerText;
             let targetted_authorname = targetted_div.children[1].innerText;
-            localStorage.setItem("Dishname", targetted_dishname);
+            localStorage.setItem("Dishnamey", targetted_dishname);
             window.location.href = "recipedetails.html";              
         })  
     })
