1c3263fa69fe605b1dfc2c2a098584d978b99221
jacques <skydive651@gmail.com>
2023-02-17 11:36:58 +0800

    changes to optimize performance

--- CreatePost.js
+++ CreatePost.js
@@ -16,7 +16,8 @@ let current_date = `${date}-${month}-${year}`
 console.log(current_date);
 
 
-$(document).ready(function (){  
+$(document).ready(function (){ 
+  console.log('loaded') 
     
   $('#createpost_Bt').click(function(e){    //java for log in page
 
@@ -70,41 +71,41 @@ $(document).ready(function (){
         console.log(response);
       });
  
-  })  
+  });  
 
 
-    $('#find_Btn').click(function(e){
-        console.log('create')
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://tutorial-9477.restdb.io/rest/recipesposts",
-            "method": "GET",
-            "headers": {
-              "content-type": "application/json",
-              "x-apikey": APIKEY,
-              "cache-control": "no-cache"
-            }
-          }
+    // $('#find_Btn').click(function(e){
+    //     console.log('create')
+    //     var settings = {
+    //         "async": true,
+    //         "crossDomain": true,
+    //         "url": "https://tutorial-9477.restdb.io/rest/recipesposts",
+    //         "method": "GET",
+    //         "headers": {
+    //           "content-type": "application/json",
+    //           "x-apikey": APIKEY,
+    //           "cache-control": "no-cache"
+    //         }
+    //       }
           
-          $.ajax(settings).done(function (response) {
-            for (let i = 0; i < response.length; i++){
-                let time = response[i].Time;
-                if (time == 5){
-                    console.log('found a post captain')
-                    let post = response[i];
+    //       $.ajax(settings).done(function (response) {
+    //         for (let i = 0; i < response.length; i++){
+    //             let time = response[i].Time;
+    //             if (time == 5){
+    //                 console.log('found a post captain')
+    //                 let post = response[i];
                     
-                    let NameOfDish = post.NameDish;
-                    let Author = post.Author;
-                    let photoCode = post.Photo
+    //                 let NameOfDish = post.NameDish;
+    //                 let Author = post.Author;
+    //                 let photoCode = post.Photo
 
-                    $("<div class=Dish><h2 class=Dish>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1></div>").insertAfter("br.start");
-                    console.log("One dish done")
-                }
-            }
-          });
+    //                 $("<div class=Dish><h2 class=Dish>" + NameOfDish + "</h2><h1 class=Author_Dish>" + Author + "</h1></div>").insertAfter("br.start");
+    //                 console.log("One dish done")
+    //             }
+    //         }
+    //       });
 
-    });
+    // });
 
 
 
@@ -180,10 +181,10 @@ $(document).ready(function (){
     // })
 
   
+  var total_points = 0;
   
-  let total_points = 0;
-
   function cal_points(callback){
+    console.log('callback')
 
     var settings = {
       "async": true,
@@ -196,14 +197,17 @@ $(document).ready(function (){
         "cache-control": "no-cache"
       }
     }
-  
-    $.ajax(settings).done(function (response) {
+
+    $.ajax(settings).done(function (response) {   //checking which posts are by the current logged in user
+
       for (let i = 0; i < response.length; i++){
+        console.log('there are ' + response.length + ' food posts to run through')
   
         let current_User = user;
         post_author = response[i].Author;
-        
+
         if (current_User == post_author){
+          console.log('user found')
           let post_likes = response[i].Likes;
   
           let starter = 0;
@@ -231,23 +235,104 @@ $(document).ready(function (){
             let result_10 = total_10 * 2;
             award_10 += result_10;
           }
-  
-          console.log(award_5);
-          console.log(award_10);
 
-          let final_award = award_5 + award_10 + starter;
-          callback(final_award)
+          let tally = award_5 + award_10 + starter;
+          total_points += tally;
+
+          // var settingsss = {
+          //   "async": true,
+          //   "crossDomain": true,
+          //   "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles",
+          //   "method": "GET",
+          //   "headers": {
+          //     "content-type": "application/json",
+          //     "x-apikey": APIKEY,
+          //     "cache-control": "no-cache"
+          //   }
+          // }
+
+          // $.ajax(settingsss).done(function (response) {      //3 accounts so the number 3 is why runs 3 times
+          //   for (let i = 0; i < response.length; i++){
+
+          //     console.log(response[i])
+          //     console.log(response.length)
+
+          //     let current_User = user;
+          //     let Userfound = response[i].Username;
+          //     console.log(response[0] + 'dddddddddddd');
+          //     let userdata = response[i];
+
+          //     if (current_User == Userfound){
+          //       console.log('userfound')
+
+
+          //       callback(final_award, current_User, Userfound, userdata);
+          //     }
+          //   }
+          // });
         }
-      }
+          else{
+            console.log('user nt found to give points')
+          }
+      }  //outside the for loop
+
+      callback()
     });
   }
 
-  cal_points(function(final_award){
-    total_points += final_award;
+  cal_points(function(){ 
     console.log(total_points)
 
-  })
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles",
+      "method": "GET",
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+      }
+    }
+    
+    $.ajax(settings).done(function (response) {
+      for (let i = 0; i < response.length; i++){
+
+        if (response[i].Username == user){
+          console.log('userfound')
+
+          let userid = response[i]._id
+          console.log(userid)
+
+          var jsondataaa = {
+            "Points": total_points,
+          }
+
+          var settingsS = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles/" + userid,
+            "method": "PUT",
+            "headers": {
+              "content-type": "application/json",
+              "x-apikey": APIKEY,
+              "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify(jsondataaa)
+          }
+      
+          $.ajax(settingsS).done(function (responseOP) {
+            console.log(responseOP);
+          });
 
 
+
+        }
+      }   
+    });          
+  });     
+
+ 
 
 }); //end coding for document
