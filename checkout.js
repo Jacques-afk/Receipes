05864940d416e694ac9952d878dfc7309a67e4d6
jacques <skydive651@gmail.com>
2023-02-18 19:30:13 +0800

    idk why I had to tell them to numbers again but ok

--- checkout.js
+++ checkout.js
@@ -1,14 +1,59 @@
+
 const APIKEY = "63db64973bc6b255ed0c456e";
-const user = localStorage.getItem("Username")
+const user = localStorage.getItem("Username");
 
 let numberofcups = localStorage.getItem("cup_no");
-console.log(numberofcups)
 let numberofplates = localStorage.getItem("plate_no");
-console.log(numberofplates)
 let numberofcookers = localStorage.getItem("cooker_no");
-console.log(numberofcookers)
-let numberofblenders = localStorage.getItem("blender_no");
-console.log(numberofblenders)
+let numberofblenders = localStorage.getItem("blender_no");      //number of times clicked starting from 0
+
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://tutorial-9477.restdb.io/rest/shopping",
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache"
+    }
+}
+  
+$.ajax(settings).done(function (response) {
+    for (let i = 0; i < response.length; i++){
+        if (response[i].User == user){
+            console.log('userfound')
+
+            console.log((+response[i].Cup) + (+numberofcups))
+
+            var jsondata = {"User": user,
+                            "Cup": (+response[i].Cup) + (+numberofcups),
+                            "Plate": (+response[i].Plate) + (+numberofplates),
+                            "Cooker": (+response[i].Cooker) + (+numberofcookers),
+                            "Blender": (+response[i].Blender) + (+numberofblenders),
+                            }
+
+
+            var settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://tutorial-9477.restdb.io/rest/shopping/" + response[i]._id,
+                "method": "PUT",
+                "headers": {
+                    "content-type": "application/json",
+                    "x-apikey": APIKEY,
+                    "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify(jsondata)
+            }
+
+            $.ajax(settings).done(function (response) {
+                console.log(response);
+            });
+        }
+    }
+});
 
 let priceCup = 560;
 let pricePlate = 350;
@@ -20,56 +65,81 @@ let picPlate = "<div class=flex-img><div class=sketchfab-embed-wrapper> <iframe 
 let picCooker = "<div class=flex-img> <div class=sketchfab-embed-wrapper> <iframe title=blender frameborder=0 allowfullscreen mozallowfullscreen=true webkitallowfullscreen=true allow=autoplay; fullscreen; xr-spatial-tracking xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src=https://sketchfab.com/models/af7b36e4999b4718817c61c9451b6e9f/embed> </iframe></div> </div>"
 let picBlender = "<div class=flex-img><div class=sketchfab-embed-wrapper> <iframe title=ricecooker frameborder=0 allowfullscreen mozallowfullscreen=true webkitallowfullscreen=true allow=autoplay; fullscreen; xr-spatial-tracking xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src=https://sketchfab.com/models/ec5265bb4b584038972fb049a4758f73/embed> </iframe></div> </div>"
 
-let totalFee = (priceCup * numberofcups) + (pricePlate * numberofplates) + (priceCooker * numberofcookers) + (priceofBlender * numberofblenders);
-console.log(totalFee);
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://tutorial-9477.restdb.io/rest/shopping",
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": APIKEY,
+      "cache-control": "no-cache"
+    }
+  }
 
-if (numberofcups > 0){
-    $("<div class=shoppingcart> <div class=everything>" + picCup + "<div class=flexinfo> <h1 class=obj_name> Recipes Cup </h1> <p class=obj_var> Variation: White </p> <p class=obj_qty> Qty: " + numberofcups + "</p> <p class=obj_price> $39 or 560 <img class='logo' src=/recicred.png> </div> </div> </div>").insertAfter(".reviewCart"); 
-}
+$.ajax(settings).done(function (response) {
+    for (let i = 0; i < response.length; i++){
+        if (response[i].User == user){
+            let gottencup = response[i].Cup;
+            let gottenplate = response[i].Plate;
+            let gottencooker = response[i].Cooker;
+            let gottenblender = response[i].Blender;
 
-if (numberofplates > 0){
-    $("<div class=shoppingcart> <div class=everything>" + picPlate + "<div class=flexinfo> <h1 class=obj_name> Recipes Plate </h1> <p class=obj_var> Variation: Orange </p> <p class=obj_qty> Qty: " + numberofplates + "</p> <p class=obj_price> $27 or 470 <img class='logo' src=/recicred.png> </div>").insertAfter(".reviewCart"); 
-}
+            let totalFee = (priceCup * gottencup) + (pricePlate * gottenplate) + (priceCooker * gottencooker) + (priceofBlender * gottenblender);
+            console.log(totalFee);
 
-if (numberofcookers > 0){
-    $("<div class=shoppingcart> <h1 class=obj_name> Recipes RiceCooker </h1> <p class=obj_var> Variation: Floral </p> <p class=obj_qty> Qty: " + numberofcookers + "</p> <p class=obj_price> $123 or 1940 <img class='logo' src=/recicred.png> </div>").insertAfter(".reviewCart"); 
-}
-
-if (numberofblenders > 0){
-    $("<div class=shoppingcart> <h1 class=obj_name> Recipes Blender </h1> <p class=obj_var> Variation: Black </p> <p class=obj_qty> Qty: " + numberofblenders + "</p> <p class=obj_price> $89 or 1400 <img class='logo' src=/recicred.png> </div>").insertAfter(".reviewCart"); 
-}
-
-$('.payreci').click(function(){
-
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles",
-        "method": "GET",
-        "headers": {
-          "content-type": "application/json",
-          "x-apikey": APIKEY,
-          "cache-control": "no-cache"
-        }
-      }
-      
-      $.ajax(settings).done(function (response) {
-        for (let i = 0; i < response.length; i++){
-            if (response[i].Username == user){
-    
-                let currentPoints = response[i].Points 
-    
-                if (currentPoints > totalFee){
-                    console.log("purchase was successful!")
-                }
-                    else{
-                        console.log("Insufficient Reci-points!")
-                    }
-    
-    
+            if (gottencup > 0){
+                $("<div class=shoppingcart> <div class=everything>" + picCup + "<div class=flexinfo> <h1 class=obj_name> Recipes Cup </h1> <p class=obj_var> Variation: White </p> <p class=obj_qty> Qty: " + numberofcups + "</p> <p class=obj_price> $39 or 560 <img class='logo' src=/recicred.png> </div> </div> </div>").insertAfter(".reviewCart"); 
             }
+            
+            if (gottenplate > 0){
+                $("<div class=shoppingcart> <div class=everything>" + picPlate + "<div class=flexinfo> <h1 class=obj_name> Recipes Plate </h1> <p class=obj_var> Variation: Orange </p> <p class=obj_qty> Qty: " + numberofplates + "</p> <p class=obj_price> $27 or 470 <img class='logo' src=/recicred.png> </div> </div> </div>").insertAfter(".reviewCart"); 
+            }
+            
+            if (gottencooker > 0){
+                $("<div class=shoppingcart> <div class=everything>" + picCooker + "<div class=flexinfo> <h1 class=obj_name> Recipes RiceCooker </h1> <p class=obj_var> Variation: Floral </p> <p class=obj_qty> Qty: " + numberofcookers + "</p> <p class=obj_price> $123 or 1940 <img class='logo' src=/recicred.png> </div> </div> </div>").insertAfter(".reviewCart"); 
+            }
+            
+            if (gottenblender > 0){
+                $("<div class=shoppingcart> <div class=everything>" + picBlender + "<div class=flexinfo> <h1 class=obj_name> Recipes Blender </h1> <p class=obj_var> Variation: Black </p> <p class=obj_qty> Qty: " + numberofblenders + "</p> <p class=obj_price> $89 or 1400 <img class='logo' src=/recicred.png> </div> </div> </div>").insertAfter(".reviewCart"); 
+            }
+
+            $('.payreci').click(function(){
+
+                var settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://tutorial-9477.restdb.io/rest/receipesprofiles",
+                    "method": "GET",
+                    "headers": {
+                      "content-type": "application/json",
+                      "x-apikey": APIKEY,
+                      "cache-control": "no-cache"
+                    }
+                  }
+                  
+                  $.ajax(settings).done(function (response) {
+                    for (let i = 0; i < response.length; i++){
+                        if (response[i].Username == user){
+                
+                            let currentPoints = response[i].Points 
+                
+                            if (currentPoints > totalFee){
+                                console.log("purchase was successful!")
+                            }
+                                else{
+                                    console.log("Insufficient Reci-points!")
+                                }
+                
+                
+                        }
+                    }
+                  });
+            });          
         }
-      });
-})
+    }
+});
+
+
 
 
